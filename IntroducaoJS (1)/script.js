d111f1b489815726e0297e97ae5c25f39a6990a9
samuccasso <samuelramoscarvalhoo@gmail.com>
2024-10-18 09:07:24 -0300

    IntruduçãoJS

---
+++ IntroducaoJS (1)/script.js
@@ -0,0 +1,14 @@
+const idade = 16
+// Crie uma variável para cada um dos 
+// Dados listados: (nome, sobrenome, telefone, estaCasado= true ou false)
+// Imprima cada um dos valores com console.log(<variavel
+
+console.log(idade) 
+const Nome = Ana
+console.log(Nome)
+const Sobrenome = Caçadora 
+console.log(Sobrenome)
+const Telefone = 
+console.log(Telefone)
+const estaCasado = "false"
+console.log(estaCasado)
