3f4fa89b9ed023ea73138950b5aa94a3a7236e69
samuccasso <samuelramoscarvalhoo@gmail.com>
2024-10-18 09:21:16 -0300

    Array01

---
+++ Array01/script.js
@@ -0,0 +1,15 @@
+//VARIÁVEIS UNITÁRIAS
+const nome = 'Aghata Nunes'
+const nome2 = 'Juju do Pix'
+const nome3 = 'Phonsy'
+// ...
+
+
+//VARIÁVEIS DE ARRAY
+const nomes = ['Aghata Nunes', 'Juju do Pix', 'Phonsy']
+
+// IMPRIMINDO OS VALORES DO ARRAY
+console.log(nomes)
+console.log(nomes[2])
+names[10] = "Ygona" // CRIANDO DINAMICAMENTE
+console.log(nomes[2])
